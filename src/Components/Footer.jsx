d39400f195262a94e7ccc1be963482a72bcb1670
Hafizul Islam <hafizul2004official@gmail.com>
2026-08-07 19:54:33 +0600

    Footer Section Update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,58 +1,107 @@
+import { FaFacebook, FaLinkedin, FaYoutube } from "react-icons/fa";
+import { FaLocationDot, FaPhone, FaEnvelope } from "react-icons/fa6";
+import { Link, NavLink } from "react-router";
+import ErrorPage from "./ErrorPage";
+// import { Link, NavLink } from "react-router";
+
 const Footer = () => {
     return (
-        <footer className="footer footer-horizontal bg-base-300 text-base-content py-9-5 p-10">
-            <nav>
-                <h6 className="footer-title">Services</h6>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Company</h6>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Social</h6>
-                <div className="grid grid-flow-col gap-4">
-                    <a>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            className="fill-current">
-                            <path
-                                d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-                        </svg>
-                    </a>
-                    <a>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            className="fill-current">
-                            <path
-                                d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-                        </svg>
-                    </a>
-                    <a>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            className="fill-current">
-                            <path
-                                d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-                        </svg>
-                    </a>
+        <footer className="bg-[#121212] text-white">
+            <div className="max-w-9/12 mx-auto px-6 py-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10">
+
+                    {/* Restaurant Info */}
+                    <div>
+                        <h2 className="text-2xl font-bold mb-5">Foodie's</h2>
+                        <p className="text-gray-500 mb-6">
+                            We serve fresh, delicious, and high-quality food made with
+                            premium ingredients. Every meal is prepared with love to create
+                            an unforgettable dining experience.
+                        </p>
+                    </div>
+
+                    <div>
+                        <h1 className="text-xl font-black">Contect Info</h1>
+                        <div className="space-y-3 mt-7">
+                            <p className="flex items-center gap-3">
+                                <FaLocationDot className="text-primary" />
+                                Savar,Dhaka, Bangladesh
+                            </p>
+
+                            <p className="flex items-center gap-3">
+                                <FaPhone className="text-primary" />
+                                +880 1921700133
+                            </p>
+
+                            <p className="flex text-sm items-center gap-3">
+                                <FaEnvelope className="text-primary" />
+                                hafizul2004official@gmail.com
+                            </p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <h2 className="text-xl font-bold mb-5">Quick Links</h2>
+
+                        <ul className="space-y-3">
+                            <li><NavLink to="/" className="hover:text-primary">Home</NavLink></li>
+                            <li><NavLink to="/about" className="hover:text-primary">About</NavLink></li>
+                            <li><NavLink to="/menu" className="hover:text-primary">Menu</NavLink></li>
+                            <li><NavLink to="/contact" className="hover:text-primary">Contact</NavLink></li>
+                            <li><NavLink to="/login" className="hover:text-primary">Login</NavLink></li>
+                        </ul>
+                    </div>
+
+                    {/* Opening Hours */}
+                    <div>
+                        <h2 className="text-xl font-bold mb-5">Opening Hours</h2>
+
+                        <div className="space-y-4">
+                            <div className="flex justify-between border-b border-base-100 pb-2">
+                                <span>Mon - Fri</span>
+                                <span>09:00 AM - 10:00 PM</span>
+                            </div>
+
+                            <div className="flex justify-between border-b border-base-100 pb-2">
+                                <span>Saturday</span>
+                                <span>10:00 AM - 11:00 PM</span>
+                            </div>
+
+                            <div className="flex justify-between border-b border-base-100 pb-2">
+                                <span>Sunday</span>
+                                <span>11:00 AM - 09:00 PM</span>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Follow Us */}
+                    <div className="flex items-center py-5 flex-col">
+                        <h2 className="text-xl items-center font-bold mb-5">Follow Us</h2>
+
+                        {/* <p className="text-gray-500 mb-6">
+              Stay connected with us for the latest offers, delicious meals,
+              and exciting updates.
+            </p> */}
+
+                        <div className=" text-center flex gap-4">
+                            <span>
+                                <Link to="https://www.facebook.com/habib20044">  <FaFacebook size={34} className="hover:text-green-500" /></Link>
+                            </span>
+                            <span>
+                                <Link to="https://www.linkedin.com/in/mdhafizulislam2004/"><FaLinkedin className="hover:text-green-500" size={34} /></Link>
+                            </span>
+                            <span><Link to="/*"><FaYoutube className="hover:text-green-500" size={34}/></Link></span>
+                        </div>
+                    </div>
                 </div>
-            </nav>
+
+                {/* Bottom Footer */}
+                <div className="border-t border-red-400 mt-12 pt-6 flex flex-col  justify-between items-center gap-4">
+
+                    <p className="text-center">
+                        &copy; 2026 Taste Nest Restaurant. All Rights Reserved.
+                    </p>
+                </div>
+            </div>
         </footer>
     );
 };
